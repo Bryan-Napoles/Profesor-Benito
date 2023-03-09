2d1cbbdf250c4ad0e6c9bdf7763f589f0a8b4644
Bryan Napoles <mcblack191@gmail.com>
2023-03-09 09:59:48 -0600

    Commit final

--- Javascript/experi.js
+++ Javascript/experi.js
@@ -9,18 +9,18 @@ function validarMaterias(event) {
     // validar si las materias seleccionadas corresponden al semestre seleccionado
     for (let i = 0; i < materiasSeleccionadas.length; i++) { 
         let semestresPermitidos = materiasSeleccionadas[i].dataset.semestre.split(' ');
-        alert(`Agregates esta materia ${materiasSeleccionadas[i].value}`);
+        alert(`Agregaste esta materia ${materiasSeleccionadas[i].value}`);
         if (!semestresPermitidos.includes(semestreSeleccionado)) {
             alert(`La materia ${materiasSeleccionadas[i].value} no está disponible para el semestre seleccionado`);
             materiasSeleccionadas[i].checked = false;
         }
     }
 
-    const formulario = document.querySelector("#formulario");
+    const formulario = document.querySelector("formulario");
 
     
     
-        const nombre = document.querySelector("#taco").value
+        const nombre = document.querySelector("taco").value
     
         const respuesta = document.getElementById("lapiz");
     
